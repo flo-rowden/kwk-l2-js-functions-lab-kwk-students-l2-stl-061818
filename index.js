66d2deca4996a18b999c9ce41a685e0b16dbe26f
Flora May Rowden <21frowden@nerinxhs.org>
2018-06-22 20:40:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function distanceFromHqInBlocks(street) {
   let blocks = street - 42
   
-  return Math.abs('feet')
+  return Math.abs(blocks)
 }
 
 function distanceFromHqInFeet(street) {
   let blocks = street - 42
   let feet = blocks * 264
+  
+  return Math.abs(feet)
 }
